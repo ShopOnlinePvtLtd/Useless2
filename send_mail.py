766b8ab27151c022fe424b230c62947ffb301b8d
Abhishek Chouhan <chouhanabhi5@gmail.com>
2021-08-16 15:25:08 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,10 +1,17 @@
-password = "thisISPAss123@@#" # BluBracketIgnore
+import os
 
-secret = "passthisISPs123@#" # BluBracketIgnore
+def main():
+  password = "thisISPAss123@@#" # BluBracketIgnore
+
+  secret = "passthisISPs123@#" # BluBracketIgnore
 
 
-secret2 = "passtfahisISPs123@#"    # BluBracketIgnore
-secret3 = "passtdhisISPs123@#"    #BluBracketIgnore
+  secret2 = "passtfahisISPs123@#"    # BluBracketIgnore
+  secret3 = "passtdhisISPs123@#"    #BluBracketIgnore
 
-# BluBracketIgnore
-secret4 = "pasdfssthisISPs123@#"
+  # BluBracketIgnore
+  secret4 = "pasdfssthisISPs123@#"
+
+
+if __name__=="__main__":
+  print("hello world")
