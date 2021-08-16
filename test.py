112d87f14de72a3ce5b30ac79135343c9500fbbe
Abhishek Chouhan <chouhanabhi5@gmail.com>
2021-08-16 12:44:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -17,4 +17,6 @@ pass_sec4 = "abhiiA$#$#123Sec" # BluBracketIgnore
 
 pass_sec5 = "abhiiA$#$#123Secingnoreme"  #BluBracketIgnore
 
+pass_sec6 = "abhiiA$##123Secingeme"  #BluBracketIgnore
+
 send(user, password)
