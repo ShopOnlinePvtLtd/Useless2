db4343638cc92228a06ebbdf3705e6ea9b644a04
Abhishek Chouhan <chouhanabhi5@gmail.com>
2021-08-16 14:39:24 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,15 +1,9 @@
 import send_email
 
 user = "abhi@test.com"
-password = "AbhiPass@321"
 
 def send(user, pass):
     send_email(user,pass)
-
-
-pass_sec = "abhiAbhiABhi310a##"
-
-pass_sec2 = "abhiiABhi310a##@@"
 
 pass_sec3 = "abhiiA$#$#123" # BluBracketIgnore
 
