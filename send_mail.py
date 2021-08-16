ca5c51b26e8c5a937066891089ce29399ced0d3e
Abhishek Chouhan <chouhanabhi5@gmail.com>
2021-08-16 15:21:24 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,3 +1,10 @@
 password = "thisISPAss123@@#" # BluBracketIgnore
 
 secret = "passthisISPs123@#" # BluBracketIgnore
+
+
+secret2 = "passtfahisISPs123@#"    # BluBracketIgnore
+secret3 = "passtdhisISPs123@#"    #BluBracketIgnore
+
+# BluBracketIgnore
+secret4 = "pasdfssthisISPs123@#"
