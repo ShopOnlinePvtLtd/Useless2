86c6e0fb933afe4676c43a5091893a8a175420be
Abhishek Chouhan <chouhanabhi5@gmail.com>
2021-08-17 12:41:20 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -4,7 +4,7 @@ def main():
   github_token = "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"
   text = "text"
   text = "text"
-  secret_new = "PassthisISPs123@#new"
+  password = "PassthisISPs123@#new"
 
   
 if __name__=="__main__":
