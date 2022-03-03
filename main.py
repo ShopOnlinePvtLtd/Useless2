614433e2f35f3d4a922cae49d64aacadd367fc5a
Abhishek Chouhan <chouhanabhi5@gmail.com>
2022-03-03 22:17:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ impoer os
 
 print('hi')
 password = "ghp_1GqDEM7yVWcNGiLKSBQq96qpAHStwT8NTErO"
+
+password = "ghp_1GqDPM4yVRcCGiMKSBQq96jfQHStwT8NTErO"
+
 print('hello there')
 
 print("i am karthik")
