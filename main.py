5777b870e0515481af490efe790396e7ab220c64
Abhishek Chouhan <chouhanabhi5@gmail.com>
2022-03-08 16:59:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ impoer os
 
 
 print('hi')
-password = "ghp_1GqDEM7yVWcNGiLKSBQq96qpAHStwT8NTErO"
+password = "ghp_1GqDEM7yVWcNGiLKSBQq96qpAHStwT8NTErO"  # BluBracketIgnore
 
-password = "ghp_1GqDPM4yVRcCGiMKSBQq96jfQHStwT8NTErO"
+password = "ghp_1GqDPM4yVRcCGiMKSBQq96jfQHStwT8NTErO"  # BluBracketIgnore
 
 
-secret = "ghp_1GqDPM4yVRcCGiMKBBQq96jfQHStgT8NTEeO"
+secret = "ghp_1GqDPM4yVRcCGiMKBBQq96jfQHStgT8NTEeO"  # BluBracketIgnore
 
 print('hello there')
 
