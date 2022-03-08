807df00f1a768520609acb3ddfabc2ecfe3453c2
Abhishek Chouhan <chouhanabhi5@gmail.com>
2022-03-08 16:58:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ password = "ghp_1GqDEM7yVWcNGiLKSBQq96qpAHStwT8NTErO"
 
 password = "ghp_1GqDPM4yVRcCGiMKSBQq96jfQHStwT8NTErO"
 
+
+secret = "ghp_1GqDPM4yVRcCGiMKBBQq96jfQHStgT8NTEeO"
+
 print('hello there')
 
 print("i am karthik")
